d1d910c5c330462f3652385217fec6679be31503
Brad Balassaitis <bradbala@bu.edu>
2023-06-07 21:26:17 -0400

    Changed Chuck Norris API code to use fetch()

--- assets/scripts/apis.js
+++ assets/scripts/apis.js
@@ -16,8 +16,20 @@
     }
 */
 
-
 function getChuckNorrisJoke() {
+    let url = 'https://api.chucknorris.io/jokes/random';
+
+    // Call the API then retrieve the JSON data and display the value property on the page
+    console.log('Calling Chuck Norris API with fetch()...');
+    fetch (url)
+        .then(response => response.json())
+        .then(data => document.getElementById('data').innerHTML = data.value || data.status + ' - ' + data.message)
+        .catch(err => document.getElementById('data').innerHTML = 'Error hitting Chuck Norris API: ' + err);
+}
+
+/*
+// Original Version, using XMLHttpRequest. Rewritten to learn to use fetch().
+function getChuckNorrisJoke_XMLHttpRequest() {
     let xhr = new XMLHttpRequest();
     xhr.onload = function () {
         if (xhr.status === 200) {
@@ -29,6 +41,7 @@ function getChuckNorrisJoke() {
     xhr.open('GET', 'https://api.chucknorris.io/jokes/random', true);
     xhr.send(null);
 }
+*/
 
 
 /* Stock Price API
